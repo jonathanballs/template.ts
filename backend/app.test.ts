b02501904be4c0378f2c9a487af971c5952f0f1d
Jonathan Balls <jonathanballs@protonmail.com>
2018-11-21 02:08:59 +0000

    Set up jest for testing

---
+++ backend/app.test.ts
@@ -0,0 +1,9 @@
+import 'jest';
+import { exportAllDeclaration } from 'babel-types';
+
+describe('Backend', () => {
+    it('renders HTML on a request to the root URL', () => {
+        expect(1).toEqual(1);
+    });
+});
+
